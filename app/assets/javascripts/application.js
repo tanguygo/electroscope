21cb74d710c0585cd55a90a9e95bdb8ab5734a29
raphaelescoyez <raphael.escoyez@gmail.com>
2014-12-09 10:45:26 +0100

    bugs fixed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-<<<<<<< HEAD
 //= require turbolinks
-//= require_tree
-=======
-
-//= require_tree  .
->>>>>>> df10f48e0e38599b7463398ca34acf4030a95805
+//= require_tree .
