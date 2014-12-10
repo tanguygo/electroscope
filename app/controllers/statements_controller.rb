8ac621db09e998446f1e41e0a2380b576eb1ca07
arnaud laurenty <arnaud.laurenty@gmail.com>
2014-12-10 09:06:11 -0800

    skipped forgery on post request

--- app/controllers/statements_controller.rb
+++ app/controllers/statements_controller.rb
@@ -2,13 +2,13 @@ class StatementsController < ApplicationController
     before_action :set_statement, only: [:show, :edit, :update]
     before_action :set_box_session, only: [:create_from_box]
 
+    skip_before_action :verify_authenticity_token, only: :create_from_box
+
       def index
         @statements=Statement.all
-
       end
 
       def create_from_box
-
         @statement = Statement.new(statement_params)
         @statement.box_session=@box_session
         @statement.save
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   resources :orders
   post "/statements/create_from_box", to: "statements#create_from_box"
-  resources :statements, only: [:index,:show,:new,:create]
+  resources :statements, only: [:index]
 
   root to: "pages#index"
   get "/:page", to: "pages#show"
