127474592f8b865471c394f8d7abda81ee3eb166
raphaelescoyez <raphael.escoyez@gmail.com>
2014-12-12 12:31:22 +0100

    useless lines deleted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,7 @@
 class ApplicationController < ActionController::Base
-  layout :layout_by_resource
   # before_action :authenticate_user!
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-
-    protected
-
-  def layout_by_resource
-    if devise_controller? && resource_name == :password && action_name == "edit"
-      "dashboard"
-    else
-      "application"
-    end
-  end
 
 end
