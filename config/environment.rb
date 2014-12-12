63364b4855342ddffea0d91053d0d871835a5f5b
raphaelescoyez <raphael.escoyez@gmail.com>
2014-12-12 10:38:13 +0100

    first save FTW

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+config.to_prepare do
+  Devise::RegistrationsController.layout proc{ |controller| user_signed_in? ? "dashboard" : "devise" }
+end
